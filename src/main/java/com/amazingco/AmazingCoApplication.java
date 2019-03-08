5aeeba6aff4b8acd733569dc14815eb8430b96f9
Natalia <n.zolotova@epages.com>
2019-03-08 18:33:44 +0100

    App config

--- src/main/java/com/amazingco/AmazingCoApplication.java
+++ src/main/java/com/amazingco/AmazingCoApplication.java
@@ -1,20 +1,19 @@
 package com.amazingco;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-import java.util.Arrays;
-
+@EntityScan
+@EnableSpringConfigured
+@SpringBootApplication
+@EnableSpringDataWebSupport
 public class AmazingCoApplication {
+
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(AmazingCoApplication.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+        new SpringApplicationBuilder(AmazingCoApplication.class).build().run(args);
     }
+
 }
