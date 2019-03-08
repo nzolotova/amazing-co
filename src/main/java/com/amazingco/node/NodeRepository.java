1744126a061176c376c2bd83b0f6ef22aef55bea
Natalia <n.zolotova@epages.com>
2019-03-08 20:10:23 +0100

    Add methods to repository

--- src/main/java/com/amazingco/node/NodeRepository.java
+++ src/main/java/com/amazingco/node/NodeRepository.java
@@ -2,7 +2,13 @@ package com.amazingco.node;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface NodeRepository extends JpaRepository<Node, UUID> {
+
+    List<Node> findByRootId(UUID id);
+
+    List<Node> findByParentId(UUID id);
+
 }
